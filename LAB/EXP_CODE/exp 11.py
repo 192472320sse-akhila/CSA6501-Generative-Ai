110f60aa0cd976659db43a6a449b51c8af7e0914
192472320sse-akhila <192472320.sse@saveetha.com>
2026-07-25 00:45:58 +0530

    Delete LAB/EXP_CODE directory

--- LAB/EXP_CODE/exp 11.py
+++
@@ -1,9 +0,0 @@
-from transformers import BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-sentence = "Machine Learning is interesting."
-
-tokens = tokenizer.tokenize(sentence)
-
-print(tokens)
--- LAB/EXP_CODE/exp 5.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import pandas
-import matplotlib
-import sklearn
-
-print("NumPy:", numpy.__version__)
-print("Pandas:", pandas.__version__)
-print("Matplotlib:", matplotlib.__version__)
-print("Scikit-learn:", sklearn.__version__)
